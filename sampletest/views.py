c748220170db4681ab5257a873a9c4d2384c7edc
kakeru-onuki <pioneerkokoro@gmail.com>
2020-01-23 02:19:12 +0900

    二回目の更新です。うまくいってるかな？

--- sampletest/views.py
+++ sampletest/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello django")
+    context={
+    "message":"welcome my sampletest",
+    "players":["勇者","戦士","魔法使い"]         
+}
+    return render(request,"sampletest/index.html",context)
+    # renderは、データとテンプレートを組み合わせて呼び返す、ショートカット関数
+    # djangoでは、renderの第三引数にcontextを渡すと、テンプレート側で呼び出すことができる。
